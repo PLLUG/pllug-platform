668a043e514f8f7a7dd17809380df1b106a97150
Ivan Lavriv <Ivan.Lavriv@irco.com>
2017-05-18 16:17:28 +0300

    Added qmqp broker and consumer

--- api/main.go
+++ api/main.go
@@ -1,8 +1,57 @@
 package main
 
-import "log"
+import (
+	"io"
+	"log"
+	"net/http"
 
+	"github.com/streadway/amqp"
+)
+
+const (
+	AMQTP_HOST = "amqp://guest:guest@192.168.99.100:5672/"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := amqp.Dial(AMQTP_HOST)
+	if err != nil {
+		log.Fatal("%s", err)
+	}
+	defer conn.Close()
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal("Error connection to channel")
+	}
+	defer ch.Close()
+	q, err := ch.QueueDeclare(
+		"test_q",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatal("Error create queue")
+	}
+	body := "Hello world"
+	err = ch.Publish(
+		"",
+		q.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	if err != nil {
+		log.Fatal("Error publish message")
+	}
+	io.WriteString(w, "hello world\n")
+}
 
 func main() {
-	log.Printf("Hello world")
+	http.HandleFunc("/", Handler)
+	log.Println("App listen on http://192.168.99.100:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- some-service/main.go
+++ some-service/main.go
@@ -1,8 +1,55 @@
 package main
 
-import "log"
+import (
+	"log"
 
+	"github.com/streadway/amqp"
+)
+
+const (
+	AMQTP_HOST = "amqp://guest:guest@192.168.99.100:5672/"
+)
 
 func main() {
-	log.Printf("Some service runned")
+	conn, err := amqp.Dial(AMQTP_HOST)
+	if err != nil {
+		log.Fatal("%s", err)
+	}
+	defer conn.Close()
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal("Error connection to channel")
+	}
+	defer ch.Close()
+	q, err := ch.QueueDeclare(
+		"test_q",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatal("Error create queue")
+	}
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatal("Error consume")
+	}
+
+forever := make(chan bool)
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+	<-forever;
 }
